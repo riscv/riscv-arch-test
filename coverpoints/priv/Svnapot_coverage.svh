7a70aa03bb4316852f7541f13d7be0433157770c
Zain2050 <zainzahid2050@gmail.com>
2025-11-04 05:15:07 -0800

    Coverpoints to test for all levels in Svnapot

--- coverpoints/priv/Svnapot_coverage.svh
+++ coverpoints/priv/Svnapot_coverage.svh
@@ -19,45 +19,60 @@ covergroup Svnapot_cg with function sample(ins_t ins);
     PTE_N_d: coverpoint ins.current.pte_d[63] {
         bins pte_N_set = {1'b1};
     }
-    PTE_Svnapot_s_i: coverpoint ins.current.pte_i[13:0] {
-        wildcard bins leaflvl_s = {14'b1000_??11?01111};
+    PTE_RWX_s_i: coverpoint ins.current.pte_i[7:0] {
+        wildcard bins leaflvl_s = {8'b11?01111};
     }
-    PTE_Svnapot_u_i: coverpoint ins.current.pte_i[13:0] {
-        wildcard bins leaflvl_u = {14'b1000_??11?11111};
+    PTE_RWX_u_i: coverpoint ins.current.pte_i[7:0] {
+        wildcard bins leaflvl_u = {8'b11?11111};
     }
-    PTE_Svnapot_s_d: coverpoint ins.current.pte_d[13:0] {
-        wildcard bins leaflvl_s = {14'b1000_??11?01111};
+    PTE_RWX_s_d: coverpoint ins.current.pte_d[7:0] {
+        wildcard bins leaflvl_s = {8'b11?01111};
     }
-    PTE_Svnapot_u_d: coverpoint ins.current.pte_d[13:0] {
-        wildcard bins leaflvl_u = {14'b1000_??11?11111};
+    PTE_RWX_u_d: coverpoint ins.current.pte_d[7:0] {
+        wildcard bins leaflvl_u = {8'b11?11111};
     }
-    PTE_Svnapot_reserved_enc_s_i: coverpoint ins.current.pte_i[22:0] {
-        wildcard bins illegal_xxx1 = {23'b????????????1_??11?01111};
-        wildcard bins illegal_xx1x = {23'b???????????1?_??11?01111};
-        wildcard bins illegal_x1xx = {23'b??????????1??_??11?01111};
-        wildcard bins illegal_0xxx = {23'b?????????0???_??11?01111};
-        wildcard bins illegal_PPNi = {23'b1000?????????_??11?01111};
+
+    PTE_Svnapot_i: coverpoint ins.current.pte_i[13:10] {
+        wildcard bins svnapot_enc = {4'b1000};
     }
-    PTE_Svnapot_reserved_enc_u_i: coverpoint ins.current.pte_i[22:0] {
-        wildcard bins illegal_xxx1 = {64'b????????????1_??11?11111};
-        wildcard bins illegal_xx1x = {64'b???????????1?_??11?11111};
-        wildcard bins illegal_x1xx = {64'b??????????1??_??11?11111};
-        wildcard bins illegal_0xxx = {64'b?????????0???_??11?11111};
-        wildcard bins illegal_PPNi = {23'b1000?????????_??11?11111};
+    PTE_Svnapot_d: coverpoint ins.current.pte_d[13:10] {
+        wildcard bins svnapot_enc = {4'b1000};
     }
-    PTE_Svnapot_reserved_enc_s_d: coverpoint ins.current.pte_d[22:0] {
-        wildcard bins illegal_xxx1 = {23'b????????????1_??11?01111};
-        wildcard bins illegal_xx1x = {23'b???????????1?_??11?01111};
-        wildcard bins illegal_x1xx = {23'b??????????1??_??11?01111};
-        wildcard bins illegal_0xxx = {23'b?????????0???_??11?01111};
-        wildcard bins illegal_PPNi = {23'b1000?????????_??11?01111};
+
+    PTE_Svnapot_reserved_enc_i: coverpoint ins.current.pte_i[13:10] {
+        wildcard bins reserved_xxx1 = {4'b???1};
+        wildcard bins reserved_xx1x = {4'b??1?};
+        wildcard bins reserved_x1xx = {4'b?1??};
+        wildcard bins reserved_0xxx = {4'b0???};
     }
-    PTE_Svnapot_reserved_enc_u_d: coverpoint ins.current.pte_d[22:0] {
-        wildcard bins illegal_xxx1 = {23'b????????????1_??11?11111};
-        wildcard bins illegal_xx1x = {23'b???????????1?_??11?11111};
-        wildcard bins illegal_x1xx = {23'b??????????1??_??11?11111};
-        wildcard bins illegal_0xxx = {23'b?????????0???_??11?11111};
-        wildcard bins illegal_PPNi = {23'b1000?????????_??11?11111};
+    PTE_Svnapot_reserved_enc_d: coverpoint ins.current.pte_d[13:10] {
+        wildcard bins reserved_xxx1 = {4'b???1};
+        wildcard bins reserved_xx1x = {4'b??1?};
+        wildcard bins reserved_x1xx = {4'b?1??};
+        wildcard bins reserved_0xxx = {4'b0???};
+    }
+
+    PageType_i: coverpoint ins.current.page_type_i {
+        `ifdef SV48
+            bins sv48_tera = {2'b11} iff (ins.current.csr[12'h180][63:60] == 4'b1001);
+            bins sv48_giga = {2'b10} iff (ins.current.csr[12'h180][63:60] == 4'b1001);
+            bins sv48_mega = {2'b01} iff (ins.current.csr[12'h180][63:60] == 4'b1001);
+        `endif
+        `ifdef SV39
+            bins sv39_giga = {2'b10} iff (ins.current.csr[12'h180][63:60] == 4'b1000);
+            bins sv39_mega = {2'b01} iff (ins.current.csr[12'h180][63:60] == 4'b1000);
+        `endif
+    }
+    PageType_d: coverpoint ins.current.page_type_d {
+        `ifdef SV48
+            bins sv48_tera = {2'b11} iff (ins.current.csr[12'h180][63:60] == 4'b1001);
+            bins sv48_giga = {2'b10} iff (ins.current.csr[12'h180][63:60] == 4'b1001);
+            bins sv48_mega = {2'b01} iff (ins.current.csr[12'h180][63:60] == 4'b1001);
+        `endif
+        `ifdef SV39
+            bins sv39_giga = {2'b10} iff (ins.current.csr[12'h180][63:60] == 4'b1000);
+            bins sv39_mega = {2'b01} iff (ins.current.csr[12'h180][63:60] == 4'b1000);
+        `endif
     }
 
     mode: coverpoint ins.current.csr[12'h180][63:60] {
@@ -71,11 +86,10 @@ covergroup Svnapot_cg with function sample(ins_t ins);
             bins sv39 = {4'b1000};
         `endif
     }
-
-    PageType_i: coverpoint ins.current.page_type_i {
+    kilopage_i: coverpoint ins.current.page_type_i {
         bins kilo = {2'b00};
     }
-    PageType_d: coverpoint ins.current.page_type_d {
+    kilopage_d: coverpoint ins.current.page_type_d {
         bins kilo = {2'b00};
     }
 
@@ -99,19 +113,26 @@ covergroup Svnapot_cg with function sample(ins_t ins);
         bins store_amo_page_fault = {32'd15} iff (ins.current.trap);
     }
 
-    Svnapot_legal_enc_exec_s:  cross PTE_Svnapot_s_i, PTE_N_i, PageType_i, mode, exec_acc, priv_mode_s;
-    Svnapot_legal_enc_exec_u:  cross PTE_Svnapot_u_i, PTE_N_i, PageType_i, mode, exec_acc, priv_mode_u;
-    Svnapot_legal_enc_read_s:  cross PTE_Svnapot_s_d, PTE_N_d, PageType_d, mode, read_acc, priv_mode_s;
-    Svnapot_legal_enc_read_u:  cross PTE_Svnapot_u_d, PTE_N_d, PageType_d, mode, read_acc, priv_mode_u;
-    Svnapot_legal_enc_write_s: cross PTE_Svnapot_s_d, PTE_N_d, PageType_d, mode, write_acc, priv_mode_s;
-    Svnapot_legal_enc_write_u: cross PTE_Svnapot_u_d, PTE_N_d, PageType_d, mode, write_acc, priv_mode_u;
+    Svnapot_legal_enc_exec_s:  cross PTE_RWX_s_i, PTE_Svnapot_i, PTE_N_i, kilopage_i, mode, exec_acc, priv_mode_s;
+    Svnapot_legal_enc_exec_u:  cross PTE_RWX_u_i, PTE_Svnapot_i, PTE_N_i, kilopage_i, mode, exec_acc, priv_mode_u;
+    Svnapot_legal_enc_read_s:  cross PTE_RWX_s_d, PTE_Svnapot_d, PTE_N_d, kilopage_d, mode, read_acc, priv_mode_s;
+    Svnapot_legal_enc_read_u:  cross PTE_RWX_u_d, PTE_Svnapot_d, PTE_N_d, kilopage_d, mode, read_acc, priv_mode_u;
+    Svnapot_legal_enc_write_s: cross PTE_RWX_s_d, PTE_Svnapot_d, PTE_N_d, kilopage_d, mode, write_acc, priv_mode_s;
+    Svnapot_legal_enc_write_u: cross PTE_RWX_u_d, PTE_Svnapot_d, PTE_N_d, kilopage_d, mode, write_acc, priv_mode_u;
 
-    Svnapot_reserved_enc_exec_s:  cross PTE_Svnapot_reserved_enc_s_i, PTE_N_i, PageType_i, mode, exec_acc, ins_page_fault, priv_mode_s;
-    Svnapot_reserved_enc_exec_u:  cross PTE_Svnapot_reserved_enc_u_i, PTE_N_i, PageType_i, mode, exec_acc, ins_page_fault, priv_mode_u;
-    Svnapot_reserved_enc_read_s:  cross PTE_Svnapot_reserved_enc_s_d, PTE_N_d, PageType_d, mode, read_acc, load_page_fault, priv_mode_s;
-    Svnapot_reserved_enc_read_u:  cross PTE_Svnapot_reserved_enc_u_d, PTE_N_d, PageType_d, mode, read_acc, load_page_fault, priv_mode_u;
-    Svnapot_reserved_enc_write_s: cross PTE_Svnapot_reserved_enc_s_d, PTE_N_d, PageType_d, mode, write_acc, store_page_fault, priv_mode_s;
-    Svnapot_reserved_enc_write_u: cross PTE_Svnapot_reserved_enc_u_d, PTE_N_d, PageType_d, mode, write_acc, store_page_fault, priv_mode_u;
+    Svnapot_reserved_enc_exec_s:  cross PTE_RWX_s_i, PTE_Svnapot_reserved_enc_i, PTE_N_i, kilopage_i, mode, exec_acc, ins_page_fault, priv_mode_s;
+    Svnapot_reserved_enc_exec_u:  cross PTE_RWX_u_i, PTE_Svnapot_reserved_enc_i, PTE_N_i, kilopage_i, mode, exec_acc, ins_page_fault, priv_mode_u;
+    Svnapot_reserved_enc_read_s:  cross PTE_RWX_s_d, PTE_Svnapot_reserved_enc_d, PTE_N_d, kilopage_d, mode, read_acc, load_page_fault, priv_mode_s;
+    Svnapot_reserved_enc_read_u:  cross PTE_RWX_u_d, PTE_Svnapot_reserved_enc_d, PTE_N_d, kilopage_d, mode, read_acc, load_page_fault, priv_mode_u;
+    Svnapot_reserved_enc_write_s: cross PTE_RWX_s_d, PTE_Svnapot_reserved_enc_d, PTE_N_d, kilopage_d, mode, write_acc, store_page_fault, priv_mode_s;
+    Svnapot_reserved_enc_write_u: cross PTE_RWX_u_d, PTE_Svnapot_reserved_enc_d, PTE_N_d, kilopage_d, mode, write_acc, store_page_fault, priv_mode_u;
+
+    Svnapot_reserved_ppni_exec_s:  cross PTE_RWX_s_i, PTE_N_i, PageType_i, exec_acc, ins_page_fault, priv_mode_s;
+    Svnapot_reserved_ppni_exec_u:  cross PTE_RWX_u_i, PTE_N_i, PageType_i, exec_acc, ins_page_fault, priv_mode_u;
+    Svnapot_reserved_ppni_read_s:  cross PTE_RWX_s_d, PTE_N_d, PageType_d, read_acc, load_page_fault, priv_mode_s;
+    Svnapot_reserved_ppni_read_u:  cross PTE_RWX_u_d, PTE_N_d, PageType_d, read_acc, load_page_fault, priv_mode_u;
+    Svnapot_reserved_ppni_write_s: cross PTE_RWX_s_d, PTE_N_d, PageType_d, write_acc, store_page_fault, priv_mode_s;
+    Svnapot_reserved_ppni_write_u: cross PTE_RWX_u_d, PTE_N_d, PageType_d, write_acc, store_page_fault, priv_mode_u;
 
 endgroup
 

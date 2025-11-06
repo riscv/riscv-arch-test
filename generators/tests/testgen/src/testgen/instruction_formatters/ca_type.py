b83d24cd278ac6a838e12fb6352f9023326a0c8d
Jordan Carlin <jordanmcarlin@gmail.com>
2025-11-05 23:52:51 -0800

    Updaate CA type and drop duplicate coverpoints

--- generators/tests/testgen/src/testgen/instruction_formatters/ca_type.py
+++ generators/tests/testgen/src/testgen/instruction_formatters/ca_type.py
@@ -11,20 +11,19 @@ from testgen.instruction_formatters.instruction_formatters import add_instructio
 from testgen.utils.common import load_int_reg, write_sigupd
 
 
-@add_instruction_formatter("CA", required_params={"rd", "rs1val", "rs2", "rs2val"})
+@add_instruction_formatter("CA", required_params={"rs1", "rs1val", "rs2", "rs2val"})
 def format_ca_type(
     instr_name: str, test_data: TestData, params: InstructionParams
 ) -> tuple[list[str], list[str], list[str]]:
     """Format CA-type instruction."""
     assert params.rs1 is not None and params.rs1val is not None
     assert params.rs2 is not None and params.rs2val is not None
-    assert params.rd is not None
     setup = [
-        load_int_reg("rs1", params.rs1, params.rs1val, test_data),
+        load_int_reg("rd/rs1", params.rs1, params.rs1val, test_data),
         load_int_reg("rs2", params.rs2, params.rs2val, test_data),
     ]
     test = [
-        f"{instr_name} x{params.rd}, x{params.rs2} # perform operation",
+        f"{instr_name} x{params.rs1}, x{params.rs2} # perform operation",
     ]
-    check = [write_sigupd(params.rd, test_data, "int")]
+    check = [write_sigupd(params.rs1, test_data, "int")]
     return (setup, test, check)

0226d82c06a1afe74d50239f380bd24462fd2a18
Jordan Carlin <jordanmcarlin@gmail.com>
2025-11-12 13:21:28 -0800

    Fix pre-commit

--- generators/tests/testgen/src/testgen/instruction_formatters/cl_type.py
+++ generators/tests/testgen/src/testgen/instruction_formatters/cl_type.py
@@ -8,8 +8,6 @@
 from testgen.data.params import InstructionParams
 from testgen.data.test_data import TestData
 from testgen.instruction_formatters.instruction_formatters import add_instruction_formatter
-from testgen.utils.common import load_int_reg, write_sigupd
-from testgen.utils.immediates import modify_imm
 
 
 @add_instruction_formatter("CL", required_params={"rd", "rs1", "immval", "temp_reg", "temp_val"})

53ff2f6c5fad6a46e071dfd0cb33c9ab3d66b222
Georgia Tai <ytai@g.hmc.edu>
2025-12-14 17:44:16 -0800

    creating rv64gcv config

--- configs/ref/sail-rv64gcv/rvtest_config.svh
+++ configs/ref/sail-rv64gcv/rvtest_config.svh
@@ -1,4 +1,4 @@
-// riscv-config.svh
+// rvtest_config.svh
 // David_Harris@hmc.edu 7 September 2024
 // SPDX-License-Identifier: Apache-2.0
 
@@ -6,7 +6,7 @@
 // It defines which extensions are enabled for that config.
 
 // Define XLEN, used in covergroups
-`define XLEN32
+`define XLEN64
 `define FLEN64
 `define VLEN512
 

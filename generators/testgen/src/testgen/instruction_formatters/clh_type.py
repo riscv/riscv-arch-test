ecb0d048676449fbf77318820a65bbc83e11ea10
Jordan Carlin <jordanmcarlin@gmail.com>
2025-12-20 16:00:41 -0800

    [ACT4] 100% coverage of Zcb

---
+++ generators/testgen/src/testgen/instruction_formatters/clh_type.py
@@ -0,0 +1,41 @@
+##################################
+# clh_type.py
+#
+# jcarlin@hmc.edu Dec 2025
+# SPDX-License-Identifier: Apache-2.0
+##################################
+
+from testgen.data.params import InstructionParams
+from testgen.data.test_data import TestData
+from testgen.instruction_formatters.instruction_formatters import add_instruction_formatter
+from testgen.utils.common import write_sigupd
+
+
+@add_instruction_formatter(
+    "CLH", required_params={"rd", "rs1", "immval", "temp_val"}, reg_range=range(8, 16), imm_bits=2, imm_signed=False
+)
+def format_clh_type(
+    instr_name: str, test_data: TestData, params: InstructionParams
+) -> tuple[list[str], list[str], list[str]]:
+    """Format CLH-type instruction."""
+    assert params.rs1 is not None
+    assert params.temp_val is not None
+    assert params.rd is not None and params.immval is not None
+
+    # Mask off bottom bit to ensure alignment
+    params.immval &= ~1
+
+    # Add value to load data region
+    test_data.add_test_data_value(params.temp_val)
+
+    setup = [
+        f"addi x{params.rs1}, x{test_data.int_regs.data_reg}, {-params.immval} # adjust base address for load",
+    ]
+    test = [
+        f"{instr_name} x{params.rd}, {params.immval}(x{params.rs1}) # perform operation",
+    ]
+    check = [
+        write_sigupd(params.rd, test_data, "int"),
+        f"addi x{test_data.int_regs.data_reg}, x{test_data.int_regs.data_reg}, SIG_STRIDE # increment data_ptr",
+    ]
+    return (setup, test, check)

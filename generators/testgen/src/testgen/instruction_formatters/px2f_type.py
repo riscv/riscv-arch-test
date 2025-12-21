ddf78f841b0d80cb43e95ff82d7eea10d89c5097
Jordan Carlin <jordanmcarlin@gmail.com>
2025-12-20 16:38:09 -0800

    [ACT4] 100% coverage of Zfa

---
+++ generators/testgen/src/testgen/instruction_formatters/px2f_type.py
@@ -0,0 +1,32 @@
+##################################
+# px2f_type.py
+#
+# jcarlin@hmc.edu Dec 2025
+# SPDX-License-Identifier: Apache-2.0
+##################################
+
+from testgen.data.params import InstructionParams
+from testgen.data.test_data import TestData
+from testgen.instruction_formatters.instruction_formatters import add_instruction_formatter
+from testgen.utils.common import load_int_reg, write_sigupd
+
+
+@add_instruction_formatter("PX2F", required_params={"fd", "rs1", "rs1val", "rs2", "rs2val"})
+def format_px2f_type(
+    instr_name: str, test_data: TestData, params: InstructionParams
+) -> tuple[list[str], list[str], list[str]]:
+    """Format PX2F-type instruction."""
+    assert params.rs1 is not None and params.rs1val is not None
+    assert params.rs2 is not None and params.rs2val is not None
+    assert params.fd is not None
+
+    setup = [
+        load_int_reg("rs1", params.rs1, params.rs1val, test_data),
+        load_int_reg("rs2", params.rs2, params.rs2val, test_data),
+        "fsflagsi 0b00000 # clear all fflags",
+    ]
+    test = [
+        f"{instr_name} f{params.fd}, x{params.rs1}, x{params.rs2} # perform operation",
+    ]
+    check = [write_sigupd(params.fd, test_data, "float")]
+    return (setup, test, check)

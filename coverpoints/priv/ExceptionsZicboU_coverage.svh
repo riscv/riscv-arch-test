7ee15afbab4e29bb3298c49c33ae07b0f202bf1a
Marina Bellido Rodriguez <mbellido@g.hmc.edu>
2025-12-27 07:36:06 -0800

    adding exceptionsZicboU

--- coverpoints/priv/ExceptionsZicboU_coverage.svh
+++ coverpoints/priv/ExceptionsZicboU_coverage.svh
@@ -17,14 +17,20 @@ covergroup ExceptionsZicboU_exceptions_cg with function sample(ins_t ins);
 
     // building blocks for the main coverpoints
     cbo_inval: coverpoint ins.current.insn {
-       wildcard bins cbo_inval = {32'b000000000000_?????_010_00000_0001111};
+        `ifdef ZICBOM_SUPPORTED
+            wildcard bins cbo_inval = {32'b000000000000_?????_010_00000_0001111};
+        `endif
     }
     cbo_flushclean: coverpoint ins.current.insn {
-        wildcard bins cbo_flush = {32'b000000000010_?????_010_00000_0001111};
-        wildcard bins cbo_clean = {32'b000000000001_?????_010_00000_0001111};
+        `ifdef ZICBOM_SUPPORTED
+            wildcard bins cbo_flush = {32'b000000000010_?????_010_00000_0001111};
+            wildcard bins cbo_clean = {32'b000000000001_?????_010_00000_0001111};
+        `endif
     }
     cbo_zero: coverpoint ins.current.insn {
-        wildcard bins cbo_zero = {32'b000000000100_?????_010_00000_0001111};
+        `ifdef ZICBOZ_SUPPORTED
+            wildcard bins cbo_zero = {32'b000000000100_?????_010_00000_0001111};
+        `endif
     }
     menvcfg_cbie: coverpoint ins.current.csr[12'h30A][5:4] {
         ignore_bins reserved = {2'b10};

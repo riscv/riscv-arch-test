aef0ad6f417527ed1119450e4bd66f458df30eba
Jordan Carlin <jordanmcarlin@gmail.com>
2026-01-07 16:29:19 -0800

    add objdump symlink for easier debugging

--- framework/src/act/makefile_gen.py
+++ framework/src/act/makefile_gen.py
@@ -141,6 +141,7 @@ def gen_rvvi_targets(test_name: Path, base_dir: Path, config: Config) -> str:
     coverage_dir = base_dir / "coverage"
     elf_dir = base_dir / "elfs"
     elf = elf_dir / test_name.with_suffix(".elf")
+    objdump_link = coverage_dir / test_name.with_suffix(".elf.objdump")
     sail_trace = coverage_dir / test_name.with_suffix(".trace")
     sail_log = coverage_dir / test_name.with_suffix(".log")
     rvvi_trace = coverage_dir / test_name.with_suffix(".rvvi")
@@ -149,6 +150,7 @@ def gen_rvvi_targets(test_name: Path, base_dir: Path, config: Config) -> str:
     return (
         "# Run test on Sail to generate log\n"
         f"{sail_trace}: {elf}\n"
+        f"\tln -s {elf}.objdump {objdump_link} # Create symlink to objdump in coverage directory for easier debugging\n"
         f"\t{config.ref_model_exe} --trace-all \\\n"
         f"\t\t--config {config.dut_include_dir}/sail.json \\\n"  # TODO: don't hardcode sail config file
         f"\t\t{elf} \\\n"

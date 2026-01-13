f24460647f5b86fe71648f00833be2ed7ba24732
Jordan Carlin <jordanmcarlin@gmail.com>
2026-01-13 09:38:50 -0800

    add csr helpers

--- generators/testgen/src/testgen/asm/__init__.py
+++ generators/testgen/src/testgen/asm/__init__.py
@@ -1,8 +1,14 @@
 # testgen/asm/__init__.py
 """Assembly code generation helpers."""
 
-from testgen.asm.csr import csr_read_test, csr_write_test
+from testgen.asm.csr import (
+    csr_access_test,
+    csr_walk_test,
+    gen_csr_read_sigupd,
+    gen_csr_write_sigupd,
+)
 from testgen.asm.helpers import (
+    comment_banner,
     load_float_reg,
     load_int_reg,
     reproducible_hash,
@@ -13,8 +19,9 @@ from testgen.asm.helpers import (
 from testgen.asm.sections import generate_test_data_section, generate_test_string_section
 
 __all__ = [
-    "csr_read_test",
-    "csr_write_test",
+    "comment_banner",
+    "csr_access_test",
+    "csr_walk_test",
     "generate_test_data_section",
     "generate_test_string_section",
     "load_float_reg",
@@ -22,5 +29,7 @@ __all__ = [
     "reproducible_hash",
     "return_test_regs",
     "to_hex",
+    "gen_csr_read_sigupd",
+    "gen_csr_write_sigupd",
     "write_sigupd",
 ]

e41ffeb28caf037a6f7c7937710cc8d5f2f256c2
David Harris <David_Harris@hmc.edu>
2026-01-25 21:40:58 -0800

    Zacas and Zabha started

--- coverpoints/priv/SsstrictSm_coverage.svh
+++ coverpoints/priv/SsstrictSm_coverage.svh
@@ -132,7 +132,8 @@ covergroup SsstrictSm_instr_cg with function sample(ins_t ins);
     cp_upperreg_imm_rs1:  cross priv_mode_m, upperreg_imm_rs1;
     cp_upperreg_fmv_rs1 : cross priv_mode_m, upperreg_fmv_rs1;
     cp_upperreg_fmv_rd :  cross priv_mode_m, upperreg_fmv_rd;
-
+    cp_amocas_odd :       cross priv_mode_m, amocas_odd;
+e
 endgroup
 
 covergroup SsstrictSm_comp_instr_cg with function sample(ins_t ins);

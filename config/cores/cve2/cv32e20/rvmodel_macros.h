56de5e6b3dd35edd659c1bae5999eeb957e3222d
David Harris <74973295+davidharrishmc@users.noreply.github.com>
2026-02-13 09:56:58 -0800

    Improved documentation, especially RVTEST Macros.  Atomic and Counter… (#928)
    … coverpoint improvements per Allen's review
    
    ---------
    
    Signed-off-by: David Harris <74973295+davidharrishmc@users.noreply.github.com>
    Co-authored-by: Jordan Carlin <jordanmcarlin@gmail.com>

--- config/cores/cve2/cv32e20/rvmodel_macros.h
+++ config/cores/cve2/cv32e20/rvmodel_macros.h
@@ -16,6 +16,9 @@
 # Perform boot operations. Can be empty.
 #define RVMODEL_BOOT
 
+# Address to use for load/store fault tests that should cause an access fault on the DUT.
+#define RVMODEL_ACCESS_FAULT_ADDRESS 0x00000000
+
 ##### TERMINATION #####
 
 # Terminate test with a pass indication.
@@ -63,27 +66,16 @@
 3:
 
 ##### Machine Timer #####
-# TODO: These MTIME macros need to be implemented for CV32E20
-# Set the machine timer (mtime) to the value in the register _R1.
-# _R2 can be used as a temporary register (e.g. address of mtime).
-# For RV32, only write the lower 32 bits of mtime and RVMODEL_SET_MTIMEH for upper 32 bits.
-#define RVMODEL_MTIME_ADDR  /* Address of mtime CSR */
-#define RVMODEL_SET_MTIME(_R1, _R2)
 
-#define RVMODEL_SET_MTIMEH(_R1, _R2)
+#define RVMODEL_MTIME_ADDR  /* Unimplemented */
 
+#define RVMODEL_MTIMECMP_ADDR  /* Unimplemented */
 
 ##### Machine Interrupts #####
 
 #define RVMODEL_SET_MEXT_INT
 
 #define RVMODEL_CLR_MEXT_INT
-
-#define RVMODEL_SET_MTIMER_INT
-
-#define RVMODEL_CLR_MTIMER_INT
-
-#define RVMODEL_SET_MTIMER_INT_SOON
 
 #define RVMODEL_SET_MSW_INT
 
@@ -95,30 +87,8 @@
 
 #define RVMODEL_CLR_SEXT_INT
 
-#define RVMODEL_SET_STIMER_INT
-
-#define RVMODEL_CLR_STIMER_INT
-
-#define RVMODEL_SET_STIMER_INT_SOON
-
 #define RVMODEL_SET_SSW_INT
 
 #define RVMODEL_CLR_SSW_INT
-
-##### Hypervisor Interrupts #####
-
-#define RVMODEL_SET_VEXT_INT
-
-#define RVMODEL_CLR_VEXT_INT
-
-#define RVMODEL_SET_VTIMER_INT
-
-#define RVMODEL_CLR_VTIMER_INT
-
-#define RVMODEL_SET_VTIMER_INT_SOON
-
-#define RVMODEL_SET_VSW_INT
-
-#define RVMODEL_CLR_VSW_INT
 
 #endif // _COMPLIANCE_MODEL_H

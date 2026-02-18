9fcd621bfc18a5b0dbbf66e8276d22e4004b6933
Ellen Yu <ellyu@g.hmc.edu>
2026-02-17 20:30:49 -0800

    Changed based on the comment: moved to march extensions

--- generators/testgen/src/testgen/priv/extensions/ExceptionsZc.py
+++ generators/testgen/src/testgen/priv/extensions/ExceptionsZc.py
@@ -38,7 +38,7 @@ def _generate_illegal_instruction_tests(test_data: TestData) -> list[str]:
     return lines
 
 
-@add_priv_test_generator("ExceptionsZc", required_extensions=["I", "Zicsr", "Zca", "Zcb", "Zcd", "Zcf", "F", "D", "Sm"])
+@add_priv_test_generator("ExceptionsZc", required_extensions=["I", "Zicsr", "Zca", "Sm"], march_extensions=["Zcb", "Zcd", "Zcf", "F", "D"])
 def make_exceptionszc(test_data: TestData) -> list[str]:
     """Generate tests for ExceptionsZc machine-mode extension."""
     lines: list[str] = []

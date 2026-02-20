4753a5fb1aabe5ccacb0289738642f323ec3800f
Jordan Carlin <jordanmcarlin@gmail.com>
2026-02-20 14:28:23 -0800

    fix lld for clang

--- framework/src/act/config.py
+++ framework/src/act/config.py
@@ -78,7 +78,8 @@ class Config(BaseModel):
     def compiler_string(self) -> str:
         """Get the compiler executable as a string with relevant flags."""
         compiler_is_clang = "clang" in self.compiler_exe.name
-        return f"{self.compiler_exe} {'--target=riscv${XLEN}' if compiler_is_clang else ''}\\\n\t\t-I{self.dut_include_dir.absolute()} \\\n\t\t-T{self.linker_script.absolute()}"
+        clang_flags = "--target=riscv${XLEN} -fuse-ld=lld"
+        return f"{self.compiler_exe} {clang_flags if compiler_is_clang else ''}\\\n\t\t-I{self.dut_include_dir.absolute()} \\\n\t\t-T{self.linker_script.absolute()}"
 
     def __str__(self) -> str:
         """Pretty print configuration."""

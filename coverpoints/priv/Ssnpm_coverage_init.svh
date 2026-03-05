11a98a6c0d5ff41c71857aeb99668fedff80ad9f
Ben Bitdiddle <ben_bitdiddle@wally.edu>
2026-03-04 19:43:40 -0800

    Just resolved minor conflict in Ssnpm_coverage_init.svh file

--- coverpoints/priv/Ssnpm_coverage_init.svh
+++ coverpoints/priv/Ssnpm_coverage_init.svh
@@ -2,11 +2,7 @@
 //
 // RISC-V Architectural Functional Coverage Covergroups Initialization File
 //
-<<<<<<< HEAD
-// Copyright (C) 2024 Harvey Mudd College, 10x Engineers, UET Lahore, Habib University
-=======
 // Copyright (C) 2026 Harvey Mudd College, 10x Engineers, UET Lahore, Habib University
->>>>>>> 4b9b1ee2 ( Added coverage file for pointer masking extension (Ssnpm))
 //
 // SPDX-License-Identifier: Apache-2.0
 //

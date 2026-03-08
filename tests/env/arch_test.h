4671307ba3cd6182fa3ccae2393ff9398ea3a18f
Julia Gong <jgong@g.hmc.edu>
2026-03-08 14:26:24 -0700

    load xcause into T5 for non-M modes

--- tests/env/arch_test.h
+++ tests/env/arch_test.h
@@ -1168,7 +1168,7 @@ spcl_\__MODE__\()chk4ecall:
 .ifnc \__MODE__ , M
         csrr    T5, CSR_XCAUSE                  // load xcause into T5 for non-M modes
 .endif
-// ***
+
 //****FIXME: what is the correct parameter register? x3=0?
 
 .ifc \__MODE__ ,  M                             // If ecall is delegated, can't go to Mmode

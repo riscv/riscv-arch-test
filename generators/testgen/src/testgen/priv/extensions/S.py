3e72f9cb217b82619500c8b4675a5c6826c1516b
Jordan Carlin <jordanmcarlin@gmail.com>
2026-03-21 20:04:56 -0700

    S formatting

--- generators/testgen/src/testgen/priv/extensions/S.py
+++ generators/testgen/src/testgen/priv/extensions/S.py
@@ -10,6 +10,7 @@
 
 from testgen.asm.csr import csr_access_test, csr_walk_test, gen_csr_read_sigupd, gen_csr_write_sigupd
 from testgen.asm.helpers import comment_banner, write_sigupd
+from testgen.constants import INDENT
 from testgen.data.state import TestData
 from testgen.priv.registry import add_priv_test_generator
 
@@ -23,12 +24,11 @@ def _generate_scause_tests(test_data: TestData) -> list[str]:
     coverpoint = "cp_scause_write_exception"
     ######################################
     lines = [
-        "",
-        f"CSRR(x{save_reg}, scause)     # save CSR before testing it",
         comment_banner(
             coverpoint,
             "with interrupt = 0: test writing each exception cause",
         ),
+        f"CSRR(x{save_reg}, scause)     # save CSR before testing it",
     ]
 
     for i in range(24):
@@ -36,7 +36,9 @@ def _generate_scause_tests(test_data: TestData) -> list[str]:
             continue
         lines.extend(
             [
-                f"LI(x{check_reg}, {i})           # exception cause {i}",
+                "",
+                f"# Testcase: set scause to exception cause {i}",
+                f"LI(x{check_reg}, {i})",
                 test_data.add_testcase(f"b_{i}", coverpoint, covergroup),
                 gen_csr_write_sigupd(check_reg, "scause", test_data),
             ]
@@ -61,7 +63,9 @@ def _generate_scause_tests(test_data: TestData) -> list[str]:
             continue
         lines.extend(
             [
-                f"LI(x{check_reg}, {i})           # interrupt cause {i}",
+                "",
+                f"# Testcase: set scause to interrupt cause {i}",
+                f"LI(x{check_reg}, {i})",
                 f"or x{check_reg}, x{check_reg}, x{temp_reg}          # set interrupt bit",
                 test_data.add_testcase(f"b_{i}", coverpoint, covergroup),
                 gen_csr_write_sigupd(check_reg, "scause", test_data),
@@ -89,9 +93,10 @@ def _generate_sstatus_sd_tests(test_data: TestData) -> list[str]:
             "sstatus.SD is read-only, so nothing should happen",
         ),
         "",
+        "# Setup",
         f"SET_MSB(x{reg1}) # put a 1 in the msb of x{reg1} (XLEN-1)",
         f"CSRR(x{save_reg}, sstatus)        # read and save sstatus",
-        f"# set up x{reg3} with sstatus except SD, FS, XS, VS cleared",
+        f"{INDENT}# set up x{reg3} with sstatus except SD, FS, XS, VS cleared",
         f"not x{reg2}, x{reg1}              # x{reg2} has all but msb set",
         f"and x{reg3}, x{save_reg}, x{reg2} # clear SD bit",
         f"LI(x{reg2}, 0x1E600)              # x{reg2} has all FS, XS, VS bits set (bits [14:13], [16:15], [10:9], respectively)",
@@ -105,7 +110,13 @@ def _generate_sstatus_sd_tests(test_data: TestData) -> list[str]:
                 for vs in range(4):
                     binname = f"sd_{sd}_fs_{fs:02b}_xs_{xs:02b}_vs_{vs:02b}"
                     fields = fs << 13 | xs << 15 | vs << 9
-                    lines.append(f"LI(x{check_reg}, 0x{fields:08x})  # fs = {fs:02b} xs = {xs:02b} vs = {vs:02b}")
+                    lines.extend(
+                        [
+                            "",
+                            f"# Testcase: set sstatus to sd = {sd}, fs = {fs:02b}, xs = {xs:02b}, vs = {vs:02b}",
+                            f"LI(x{check_reg}, 0x{fields:08x})  # fs = {fs:02b} xs = {xs:02b} vs = {vs:02b}",
+                        ]
+                    )
                     if sd == 1:
                         lines.append(f"or x{check_reg}, x{check_reg}, x{reg1}      # set SD bit")
                     lines.extend(
@@ -132,17 +143,23 @@ def _generate_priv_inst_tests(test_data: TestData) -> list[str]:
             coverpoint,
             "Executing ecall and ebreak and mret should cause an exception",
         ),
+        "",
         # ecall test
+        "# Testcase: ecall instruction",
         test_data.add_testcase("ecall", coverpoint, covergroup),
-        "ecall                 # test ecall instruction",
+        "ecall               # test ecall instruction",
         "nop                 # trap handler skips following instruction so this should not be executed",
+        "",
         # ebreak test
+        "# Testcase: ebreak instruction",
         test_data.add_testcase("ebreak", coverpoint, covergroup),
-        "ebreak                # test ebreak instruction",
+        "ebreak              # test ebreak instruction",
         "nop                 # trap handler skips following instruction so this should not be executed",
+        "",
         # mret test
+        "# Testcase: mret instruction",
         test_data.add_testcase("mret", coverpoint, covergroup),
-        "mret                  # test mret instruction",
+        "mret                # test mret instruction",
         "nop                 # trap handler skips following instruction so this should not be executed",
     ]
 
@@ -163,8 +180,9 @@ def _generate_mretm_tests(test_data: TestData) -> list[str]:
             "Execute mret while sweeping cross-product of mpp, mprv, mpie, mie",
         ),
         "",
+        "# Setup",
         f"CSRR(x{save_reg}, mstatus)        # read and save mstatus",
-        f"# set up x{reg1} with mstatus except MPP, MPRV, MPIE, MIE cleared",
+        f"{INDENT}# set up x{reg1} with mstatus except MPP, MPRV, MPIE, MIE cleared",
         f"LI(x{reg2}, 0x21888)          # x{reg2} has all MPP, MPRV, MPIE, MIE bits set (bits [12:11], [17], [7], [3], respectively)",
         f"not x{reg2}, x{reg2}              # x{reg2} has all but MPP, MPRV, MPIE, MIE bits set",
         f"and x{reg1}, x{save_reg}, x{reg2}          # clear MPP, MPRV, MPIE, MIE bits",
@@ -179,6 +197,8 @@ def _generate_mretm_tests(test_data: TestData) -> list[str]:
 
                     lines.extend(
                         [
+                            "",
+                            f"# Testcase: mret with mpp = {mpp:02b}, mprv = {mprv}, mpie = {mpie}, mie = {mie}",
                             # Test the write value
                             f"LI(x{check_reg}, 0x{fields:08x})  # mpp = {mpp:02b} mprv = {mprv} mpie = {mpie} mie = {mie}",
                             f"or x{check_reg}, x{check_reg}, x{reg1}          # value to write to mstatus with MPP/MPRV/MPIE/MIE bits set/clear",
@@ -217,8 +237,9 @@ def _generate_sretm_tests(test_data: TestData) -> list[str]:
             "MPRV <- 0. SPP <- 0 (U-mode).  TSR has no effect.",
         ),
         "",
+        "# Setup",
         f"CSRR(x{save_reg}, mstatus)        # read and save mstatus",
-        f"# set up x{reg1} with mstatus except MPRV, SPP, SPIE, SIE, TSR cleared",
+        f"{INDENT}# set up x{reg1} with mstatus except MPRV, SPP, SPIE, SIE, TSR cleared",
         f"LI(x{reg2}, 0x420122)          # x{reg2} has all MPRV, SPP, SPIE, SIE, TSR bits set (bits [17], [8], [5], [1], [22] respectively)",
         f"not x{reg2}, x{reg2}              # x{reg2} has all but MPRV, SPP, SPIE, SIE, TSR bits set",
         f"and x{reg1}, x{save_reg}, x{reg2}          # clear MPRV, SPP, SPIE, SIE, TSR bits",
@@ -234,6 +255,8 @@ def _generate_sretm_tests(test_data: TestData) -> list[str]:
 
                         lines.extend(
                             [
+                                "",
+                                f"# Testcase: sret from m-mode with spp = {spp}, mprv = {mprv}, spie = {spie}, sie = {sie}, tsr = {tsr}",
                                 # Test the write value
                                 f"LI(x{check_reg}, 0x{fields:08x}) # mprv = {mprv} spp = {spp} spie = {spie} sie = {sie} tsr = {tsr}",
                                 f"or x{check_reg}, x{check_reg}, x{reg1}          # value to write to mstatus with MPRV/SPP/SPIE/SIE/TSR bits set/clear",
@@ -272,8 +295,9 @@ def _generate_srets_tests(test_data: TestData) -> list[str]:
             "MPRV <- 0. SPP <- 0 (U-mode).  TSR causes illegal instruction.",
         ),
         "",
+        "# Setup",
         f"CSRR(x{save_reg}, sstatus)        # read and save sstatus",
-        f"# set up x{reg1} with sstatus except SPP, SPIE, SIE cleared",
+        f"{INDENT}# set up x{reg1} with sstatus except SPP, SPIE, SIE cleared",
         f"LI(x{reg2}, 0x122)          # x{reg2} has all SPP, SPIE, SIE bits set (bits [8], [5], [1] respectively)",
         f"not x{reg2}, x{reg2}              # x{reg2} has all but SPP, SPIE, SIE bits set",
         f"and x{reg1}, x{save_reg}, x{reg2}          # clear SPP, SPIE, SIE bits",
@@ -283,6 +307,8 @@ def _generate_srets_tests(test_data: TestData) -> list[str]:
         lines.extend(
             [
                 # Set mstatus.TSR from M-mode
+                "",
+                "# Set mstatus.TSR",
                 "RVTEST_GOTO_MMODE      # enter machine mode for twiddling mstatus.TSR",
                 f"LI(x{check_reg}, {1 << 22})  # mstatus.TSR bit",
             ]
@@ -302,6 +328,8 @@ def _generate_srets_tests(test_data: TestData) -> list[str]:
 
                     lines.extend(
                         [
+                            "",
+                            f"# Testcase: sret from s-mode with spp = {spp}, spie = {spie}, sie = {sie}, tsr = {tsr}",
                             # Test the write value
                             f"LI(x{check_reg}, 0x{fields:08x}) # spp = {spp} spie = {spie} sie = {sie}",
                             f"or x{check_reg}, x{check_reg}, x{reg1}          # value to write to sstatus with SPP/SPIE/SIE bits set/clear",
@@ -355,15 +383,15 @@ def _generate_scsr_tests(test_data: TestData) -> list[str]:
         ),
     )
 
-    lines.extend(["#ifdef F_SUPPORTED"])
+    lines.extend(["", "#ifdef F_SUPPORTED"])
     for csr in ["fflags", "frm", "fcsr"]:
         lines.extend(csr_access_test(test_data, csr, covergroup, coverpoint))
-    lines.extend(["#endif"])
+    lines.extend(["", "#endif"])
 
-    lines.extend(["#ifdef V_SUPPORTED"])
+    lines.extend(["", "#ifdef V_SUPPORTED"])
     for csr in ["vstart", "vxsat", "vxrm", "vcsr", "vl", "vtype", "vlenb"]:
         lines.extend(csr_access_test(test_data, csr, covergroup, coverpoint))
-    lines.extend(["#endif"])
+    lines.extend(["", "#endif"])
 
     ######################################
     coverpoint = "cp_scsrwalk"
@@ -392,6 +420,7 @@ def _generate_scsr_tests(test_data: TestData) -> list[str]:
 
     lines.extend(
         [
+            "# CSR Walk Tests for satp",
             "csrw satp, zero      # set satp to 0 to start with",
             f"LI(x{mask_reg}, -1)     # x{mask_reg} = all 1s for walking bit tests",
             f"srli x{mask_reg}, x{mask_reg}, 4    # change 4 msbs to 0s to exclude satp.mode from RV64 walk tests",
@@ -404,6 +433,7 @@ def _generate_scsr_tests(test_data: TestData) -> list[str]:
     for i in range(60):
         lines.extend(
             [
+                "",
                 f"csrs satp, x{walk_reg}    # set bit {i} in satp",
                 test_data.add_testcase(f"bit_{i}_set", coverpoint, covergroup),
                 gen_csr_read_sigupd(check_reg, "satp", test_data),
@@ -432,6 +462,8 @@ def _generate_scsr_tests(test_data: TestData) -> list[str]:
     ):
         lines.extend(
             [
+                "",
+                f"# Testcase: attempt to access CSR 0x{csr:03x}",
                 test_data.add_testcase(f"{csr}", coverpoint, covergroup),
                 f"CSRR(t0, 0x{csr:03x})    # attempt to read higher-privilege CSR {csr:03x}; should get illegal instruction",
             ]
@@ -449,12 +481,14 @@ def _generate_scsr_tests(test_data: TestData) -> list[str]:
     )
     r1 = test_data.int_regs.get_register(exclude_regs=[0])
 
-    lines.append(f"LI(x{r1}, -1)          # x{r1} = all 1s\n")
+    lines.append(f"LI(x{r1}, -1)          # x{r1} = all 1s")
     for csr in range(0xC00, 0xF00):
         lines.extend(
             [
+                "",
+                f"# Testcase: attempt to access CSR 0x{csr:03x}",
                 test_data.add_testcase(f"{csr}", coverpoint, covergroup),
-                f"CSRW(0x{csr:03x}, x{r1})    # attempt to write read-only CSR {csr:03x}; should get illegal instruction\n",
+                f"CSRW(0x{csr:03x}, x{r1})    # attempt to write read-only CSR {csr:03x}; should get illegal instruction",
             ]
         )
     test_data.int_regs.return_register(r1)
@@ -483,13 +517,17 @@ def _generate_scsr_tests(test_data: TestData) -> list[str]:
         ),
     )
     r1, r2 = test_data.int_regs.get_registers(2, exclude_regs=[0])
-    lines.append(f"LI(x{r1}, -1)          # x{r1} = all 1s for writing to shadowed registers\n")
-    lines.append(_add_shadow(r1, r2, "mstatus", "sstatus", coverpoint, covergroup, test_data))
-    lines.append(_add_shadow(r1, r2, "mie", "sie", coverpoint, covergroup, test_data))
-    lines.append(_add_shadow(r1, r2, "mip", "sip", coverpoint, covergroup, test_data))
-    lines.append(_add_shadow(r1, r2, "sstatus", "mstatus", coverpoint, covergroup, test_data))
-    lines.append(_add_shadow(r1, r2, "sie", "mie", coverpoint, covergroup, test_data))
-    lines.append(_add_shadow(r1, r2, "sip", "mip", coverpoint, covergroup, test_data))
+    lines.extend(
+        [
+            f"LI(x{r1}, -1)          # x{r1} = all 1s for writing to shadowed registers",
+            _add_shadow(r1, r2, "mstatus", "sstatus", coverpoint, covergroup, test_data),
+            _add_shadow(r1, r2, "mie", "sie", coverpoint, covergroup, test_data),
+            _add_shadow(r1, r2, "mip", "sip", coverpoint, covergroup, test_data),
+            _add_shadow(r1, r2, "sstatus", "mstatus", coverpoint, covergroup, test_data),
+            _add_shadow(r1, r2, "sie", "mie", coverpoint, covergroup, test_data),
+            _add_shadow(r1, r2, "sip", "mip", coverpoint, covergroup, test_data),
+        ]
+    )
     test_data.int_regs.return_registers([r1, r2])
 
     return lines
@@ -500,28 +538,35 @@ def _add_shadow(r1: int, r2: int, wreg: str, rreg: str, coverpoint: str, covergr
     return str.join(
         "\n",
         [
+            "",
+            f"# Testcase: shadow CSR test for writing {wreg} and reading {rreg}",
             f"csrw {wreg}, x{r1}       # write all 1s to {wreg}",
             test_data.add_testcase(f"{wreg}_{rreg}_1s", coverpoint, covergroup),
             gen_csr_read_sigupd(r2, rreg, test_data),
             f"csrw {wreg}, x0       # write all 0s to {wreg}",
             test_data.add_testcase(f"{wreg}_{rreg}_0s", coverpoint, covergroup),
             gen_csr_read_sigupd(r2, rreg, test_data),
-            "",
         ],
     )
 
 
-@add_priv_test_generator("S", required_extensions=["Sm", "S", "Zicsr"])
+@add_priv_test_generator("S", required_extensions=["S", "Zicsr"])
 def make_s(test_data: TestData) -> list[str]:
     """Generate tests for S supervisor-mode testsuite."""
     lines: list[str] = []
 
-    lines.extend(["# Run some tests in machine mode"])
+    lines.append("### Run some tests in machine mode")
     lines.extend(_generate_mretm_tests(test_data))
     lines.extend(_generate_sretm_tests(test_data))
     lines.extend(_generate_srets_tests(test_data))
-    lines.extend(["RVTEST_GOTO_MMODE  # Get back to machine mode to prepare to go to supervisor mode"])
-    lines.extend(["RVTEST_GOTO_LOWER_MODE Smode  # Run most tests in supervisor mode"])
+    lines.extend(
+        [
+            "",
+            "",
+            "RVTEST_GOTO_MMODE  # Get back to machine mode to prepare to go to supervisor mode",
+            "RVTEST_GOTO_LOWER_MODE Smode  # Run remaining tests in supervisor mode",
+        ]
+    )
     lines.extend(_generate_scause_tests(test_data))
     lines.extend(_generate_sstatus_sd_tests(test_data))
     lines.extend(_generate_priv_inst_tests(test_data))

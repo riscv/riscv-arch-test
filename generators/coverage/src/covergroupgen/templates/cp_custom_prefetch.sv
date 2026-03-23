cadee8688e421a391dc0e185e496880068590620
David Harris <David_Harris@hmc.edu>
2026-03-23 01:19:13 -0700

    Fixed coverpoint name

--- generators/coverage/src/covergroupgen/templates/cp_custom_prefetch.sv
+++ generators/coverage/src/covergroupgen/templates/cp_custom_prefetch.sv
@@ -1,6 +1,6 @@
     // Custom coverpoints for cbo.  Coverpoint just checks that instruction was executed
     // but test is more comprehensive, writing and reading back consecutive words
 
-    cp_custom_cbo : coverpoint ins.current.insn  {
+    cp_custom_prefetch : coverpoint ins.current.insn  {
         wildcard bins prefetch = {32'b???????000???????110000000010011};
     }

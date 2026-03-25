d5273847c28440f2c5e693fb24c8422194f66973
karabambus <mradic07@gmail.com>
2026-03-25 13:43:09 +0100

    cv32e40p: clear mcountinhibit in RVMODEL_BOOT
    CV32E40P resets mcountinhibit=0xd (all counters inhibited).
    Use raw csrwi encoding to avoid -march dependency on Zicsr.

--- config/cores/cv32e40p/rvmodel_macros.h
+++ config/cores/cv32e40p/rvmodel_macros.h
@@ -13,8 +13,12 @@
 
 ##### STARTUP #####
 
-# Perform boot operations. Can be empty.
-#define RVMODEL_BOOT
+# Perform boot operations.
+# CV32E40P resets mcountinhibit=0xd (all counters inhibited). Clear it so
+# cycle/instret increment as Zicntr tests expect.
+# Raw encoding: .word 0x32005073 = csrwi mcountinhibit, 0
+#define RVMODEL_BOOT \
+  .word 0x32005073 ;
 
 # Address to use for load/store fault tests that should cause an access fault on the DUT.
 #define RVMODEL_ACCESS_FAULT_ADDRESS 0x00000000

3230f9be28304ca8af085dbed2ad9f47e1a8895a
AmanMurad <aman.murad6600@gmail.com>
2026-03-31 13:18:38 +0500

    Add suggested changed by the tech lead

--- generators/coverage/src/covergroupgen/templates/cmp_rd_rs1_pair_full_val.sv
+++
@@ -1,5 +0,0 @@
-    cmp_rd_rs1_pair_full_val : coverpoint (ins.current.rd_val == ins.prev.rd_val) iff (ins.trap == 0) {
-        // Compare RD current to RD previous value which is basically rs1 current value for the entire width of the register pair
-        bins rd_pair_equal_val_rs1 = {1};
-        bins rd_pair_not_equal_val_rs1 = {0};
-    }

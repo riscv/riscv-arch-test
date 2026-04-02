9c708b748182a5acf07a0b08ca49b0e7ced7e712
umershahidengr <umer.shahid@10xengineers.ai>
2026-04-02 22:36:35 +0500

    Updated qemu configs to add hpm counter infos and fixed some bugs which are causing CI to hang

--- config/qemu/qemu-RVI20U64/rvtest_config.svh
+++ config/qemu/qemu-RVI20U64/rvtest_config.svh
@@ -12,4 +12,28 @@
 // Define relevant addresses
 `define CLINT_BASE 64'h02000000
 
+// PMP Grain (G) — QEMU default grain is 0
+`define G 0
+`define G_IS_0
+
+// PMP mode selection
+`define PMP_16
+
+// Base addresses specific for PMP
+`define RAM_BASE_ADDR       64'h80000000
+`define LARGEST_PROGRAM     64'h00001000
+
+`define RVMODEL_ACCESS_FAULT_ADDRESS 64'h00000000
+
 //define extra supported extensions to collect full coverage in Privileged files
+`define D_SUPPORTED
+`define F_SUPPORTED
+`define ZIHPM_SUPPORTED
+`define ZCA_SUPPORTED
+`define ZCD_SUPPORTED
+`define ZAAMO_SUPPORTED
+`define ZALRSC_SUPPORTED
+
+`define COUNTINHIBIT_EN_0
+`define COUNTINHIBIT_EN_2
+`define TIME_CSR_IMPLEMENTED

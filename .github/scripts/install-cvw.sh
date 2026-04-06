70809c77dd33d9cc17afded02a1e3d8594c57035
Jordan Carlin <jordanmcarlin@gmail.com>
2026-04-06 10:05:01 -0700

    Add CVW to CI

---
+++ .github/scripts/install-cvw.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0
+# Jordan Carlin jcarlin@hmc.edu April 2026
+# Install CVW (CORE-V Wally) and Verilator for CI
+# Usage: install-cvw.sh <install-dir>
+# Update commits/versions to rebuild (cache key is derived from this script's hash).
+
+set -euo pipefail
+
+INSTALL_DIR="${1:?Usage: install-cvw.sh <install-dir>}"
+CVW_COMMIT="5373252f79753e2de9501f002a5640e2b669db55"
+VERILATOR_VERSION="v5.036"
+
+# Install Verilator from source
+git clone https://github.com/verilator/verilator.git
+cd verilator
+git checkout "$VERILATOR_VERSION"
+autoconf
+./configure --prefix="$INSTALL_DIR"
+make -j"$(nproc)"
+make install
+cd ..
+
+# Clone CVW repo
+git clone https://github.com/openhwgroup/cvw.git "$INSTALL_DIR/cvw"
+cd "$INSTALL_DIR/cvw"
+git checkout "$CVW_COMMIT"

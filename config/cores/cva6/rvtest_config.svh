163220e4655d4383f10a805fd5b7ab51eaec3229
arshiarifat-10x <arshia.rifat@10xengineers.ai>
2026-04-17 12:49:24 +0500

    updated files

--- config/cores/cva6/rvtest_config.svh
+++ config/cores/cva6/rvtest_config.svh
@@ -1,16 +1,15 @@
-// rvtest_config.svh 
+// rvtest_config.svh
 // cv32a65x configuration
 // SPDX-License-Identifier: Apache-2.0
 
 `define XLEN32
 
-//PMP not supported in CV32A65X
+// PMP not supported in CV32A65X
 // `define PMP_16
 
-// Base addresses 
+// Base addresses
 `define RAM_BASE_ADDR       32'h80000000
 `define LARGEST_PROGRAM     32'h00010000
 
-`define ACCESS_FAULT_ADDRESS 32'h50000000
+// `define RVMODEL_ACCESS_FAULT_ADDRESS 32'h50000000
 `define CLINT_BASE 32'h02000000 
-

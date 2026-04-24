eff5af078fe5f81a1dece4bfa2809e3fdee97a8f
Ben Bitdiddle <ben_bitdiddle@wally.edu>
2026-04-23 22:48:46 -0700

    Applied range for coveing offsets

--- coverpoints/priv/Za64rs_coverage.svh
+++ coverpoints/priv/Za64rs_coverage.svh
@@ -19,33 +19,14 @@ covergroup Za64rs_cg with function sample(ins_t ins);
     lr_w_instr: coverpoint ins.prev.insn {
         wildcard bins lr_w = {LR_W};
     }
-
     sc_w_instr: coverpoint ins.current.insn {
         wildcard bins sc_w = {SC_W};
     }
-
     lr_w_base_aligned: coverpoint ins.prev.rs1_val[5:0] {
         bins aligned_64 = {6'd0};
     }
-
     sc_offset: coverpoint ins.current.rs1_val[6:0] {
-        bins offset_0  = {7'd0};
-        bins offset_4  = {7'd4};
-        bins offset_8  = {7'd8};
-        bins offset_12 = {7'd12};
-        bins offset_16 = {7'd16};
-        bins offset_20 = {7'd20};
-        bins offset_24 = {7'd24};
-        bins offset_28 = {7'd28};
-        bins offset_32 = {7'd32};
-        bins offset_36 = {7'd36};
-        bins offset_40 = {7'd40};
-        bins offset_44 = {7'd44};
-        bins offset_48 = {7'd48};
-        bins offset_52 = {7'd52};
-        bins offset_56 = {7'd56};
-        bins offset_60 = {7'd60};
-        bins offset_64 = {7'd64};
+         bins offset_aligned[] = {[7'd0 : 7'd64]} with (item % 4 == 0);
     }
 
     cp_za64rs: cross  sc_w_instr, sc_offset, lr_w_instr, lr_w_base_aligned ;
--- coverpoints/priv/Zic64bzicboz_coverage.svh
+++ coverpoints/priv/Zic64bzicboz_coverage.svh
@@ -18,29 +18,11 @@ covergroup Zic64bzicboz_cg with function sample(ins_t ins);
     cbo_zero: coverpoint ins.current.insn {
         wildcard bins cbo_zero = {CBO_ZERO};
     }
-
     cbo_zero_offset: coverpoint ins.current.rs1_val[6:0] {
-        bins offset_0  = {7'd0};
-        bins offset_4  = {7'd4};
-        bins offset_8  = {7'd8};
-        bins offset_12 = {7'd12};
-        bins offset_16 = {7'd16};
-        bins offset_20 = {7'd20};
-        bins offset_24 = {7'd24};
-        bins offset_28 = {7'd28};
-        bins offset_32 = {7'd32};
-        bins offset_36 = {7'd36};
-        bins offset_40 = {7'd40};
-        bins offset_44 = {7'd44};
-        bins offset_48 = {7'd48};
-        bins offset_52 = {7'd52};
-        bins offset_56 = {7'd56};
-        bins offset_60 = {7'd60};
-        bins offset_64 = {7'd64};
+        bins offset_aligned[] = {[7'd0 : 7'd64]} with (item % 4 == 0);
     }
 
     cp_zi64cb: cross cbo_zero, cbo_zero_offset ;
-
 
 endgroup
 

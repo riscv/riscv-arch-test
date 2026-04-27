d4fc2e396ed03f446bc7b83ef001b6d03ab35405
ayeshaanwaar05 <ayesha.anwaar2005@gmail.com>
2026-04-26 22:02:40 -0700

    Add csr_access_test

--- generators/testgen/src/testgen/priv/extensions/Sscounterenw.py
+++ generators/testgen/src/testgen/priv/extensions/Sscounterenw.py
@@ -4,7 +4,7 @@
 # Scounteren supervisor counter-enable register test generator.
 # SPDX-License-Identifier: Apache-2.0
 ##################################
-from testgen.asm.csr import csr_walk_test
+from testgen.asm.csr import csr_access_test, csr_walk_test
 from testgen.asm.helpers import comment_banner
 from testgen.data.state import TestData
 from testgen.priv.registry import add_priv_test_generator
@@ -25,7 +25,7 @@ def _generate_scounteren_tests(test_data: TestData) -> list[str]:
     ]
 
     lines.extend(csr_walk_test(test_data, "scounteren", covergroup, coverpoint))
-
+    lines.extend(csr_access_test(test_data, "scounteren", covergroup, coverpoint))
     lines.extend(
         [
             "",

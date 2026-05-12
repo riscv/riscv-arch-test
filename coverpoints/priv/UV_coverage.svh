dbb3ab1ab0f1a85f5b90616c1a9b174b4dd4619c
James Cassidy <jkc.cassidy@gmail.com>
2026-05-12 13:41:56 -0700

    UV wildcard added to bins

--- coverpoints/priv/UV_coverage.svh
+++ coverpoints/priv/UV_coverage.svh
@@ -30,8 +30,8 @@ covergroup UV_cg with function sample(ins_t ins);
     }
 
     csrop: coverpoint ins.current.insn[14:12] iff (ins.current.insn[6:0] == 7'b1110011) {
-        bins csrrs = {CSRRS};
-        bins csrrc = {CSRRC};
+        wildcard bins csrrs = {CSRRS};
+        wildcard bins csrrc = {CSRRC};
     }
 
     csraccesses : coverpoint ins.current.insn {

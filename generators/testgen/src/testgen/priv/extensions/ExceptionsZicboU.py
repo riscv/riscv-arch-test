677034dca45dc76567881eaa9bb09cf14a6ab047
David Harris <David_Harris@hmc.edu>
2026-05-13 06:52:24 -0700

    fix outdated base files

--- generators/testgen/src/testgen/priv/extensions/ExceptionsZicboU.py
+++ generators/testgen/src/testgen/priv/extensions/ExceptionsZicboU.py
@@ -303,7 +303,7 @@ def _generate_cbo_misaligned_tests(test_data: TestData) -> list[str]:
 @add_priv_test_generator(
     "ExceptionsZicboU",
     required_extensions=["U"],
-    march_extensions=["Zicsr", "Zicbom", "Zicboz", "Zicbop"],
+    march_extensions=["Zicbom", "Zicboz", "Zicbop"],
 )
 def make_exceptionszicbou(test_data: TestData) -> list[str]:
     """Generate tests for ExceptionsZicboU coverpoints"""

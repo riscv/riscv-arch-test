92cdc21a531c45cd38385b373baf538cfc68730e
karabambus <mradic07@gmail.com>
2026-05-21 20:08:01 +0200

    fix(cv32e40x): mtvec 128-byte aligned, MCOUNTENABLE_EN all false, PMA_GRANULARITY verified

--- config/cores/cve4/cv32e40x-rv32imc/rvmodel_macros.h
+++ config/cores/cve4/cv32e40x-rv32imc/rvmodel_macros.h
@@ -64,9 +64,9 @@
 ##### Interrupt Latency #####
 
 #define RVMODEL_INTERRUPT_LATENCY 10
-// CV32E40X mtvec WARL forces BASE[7:2]=0; BASE is 256-byte aligned, not 128.
-// User Manual control_status_registers.rst csr-mtvec: BASE[31:8] RW, BASE[7:2] WARL(0x0).
-#define RVMODEL_MTVEC_ALIGN 8
+// CV32E40X mtvec: BASE[31:7] WARL (writable), BASE[6:2] WARL(0x0) hardwired 0.
+// User Manual csr-mtvec: "always aligned to 128 bytes." RTL CSR_BASIC_MTVEC_MASK=0xFFFFFF81 (bit 7 writable).
+#define RVMODEL_MTVEC_ALIGN 7
 
 ##### Machine Timer #####
 

6d1b2e13d400859aca1bac40a9424d178a2770d2
ayeshaanwaar05 <ayesha.anwaar2005@gmail.com>
2026-05-27 13:13:20 -0700

    Added suggested coverpoints

--- coverpoints/priv/Ssstateen_coverage.svh
+++ coverpoints/priv/Ssstateen_coverage.svh
@@ -99,6 +99,99 @@ covergroup Ssstateen_cg with function sample(ins_t ins);
                 ignore_bins ig2 = binsof(misa_F.F_clear) && binsof(sstateen0_fcsr_bit.fcsr_zero);
         }
     `endif
+    `ifdef IMSIC_SUPPORTED
+        imsic_state: coverpoint get_csr_val(ins.hart, ins.issue, `SAMPLE_CURRENT, "mstateen0", "imsic") {
+            bins imsic_disabled = {1'b0};
+            bins imsic_enabled  = {1'b1};
+        }
+        imsic_csrs: coverpoint ins.current.insn[31:20] {
+            wildcard bins stopei = {CSR_STOPEI};
+            wildcard bins vstopei = {CSR_VSTOPEI};
+        }
+    `endif
+    `ifdef AIA_SUPPORTED
+        aia_state: coverpoint get_csr_val(ins.hart, ins.issue, `SAMPLE_CURRENT, "mstateen0", "aia") {
+            bins aia_disabled = {1'b0};
+            bins aia_enabled  = {1'b1};
+        }
+        aia_csrs: coverpoint ins.current.insn[31:20] {
+            `ifdef XLEN64
+                wildcard bins aia_m = {CSR_SIE};
+                wildcard bins aia_s = {CSR_SIP};
+            `endif
+            `ifdef XLEN32
+                bins aia_m = {CSR_SIEH};
+                bins aia_s = {CSR_SIPH};
+            `endif
+        }
+    `endif
+    `ifdef UDB_MXLEN_64
+        envcfg_state: coverpoint ins.current.csr[CSR_MSTATEEN0][62] {
+                bins envcfg_disabled = {1'b0};
+                bins envcfg_enabled  = {1'b1};
+        }
+    `else
+        envcfg_state: coverpoint ins.current.csr[CSR_MSTATEEN0H][30] {
+                bins envcfg_disabled = {1'b0};
+                bins envcfg_enabled  = {1'b1};
+        }
+    `endif
+    senvcfg_csr: coverpoint ins.current.insn[31:20] {
+                bins senvcfg = {CSR_SENVCFG};
+    }
+    `ifdef SSDTRIG_SUPPORTED
+    context_state: coverpoint get_csr_val(ins.hart, ins.issue, `SAMPLE_CURRENT, "mstateen0", "context") {
+        bins context_disabled = {1'b0};
+        bins context_enabled  = {1'b1};
+    }
+    scontext_csr: coverpoint ins.current.insn[31:20] {
+        wildcard bins scontext = {CSR_SCONTEXT};
+    }
+`endif
+
+    // ── Sm1p13 + Hypervisor dependent coverpoints (cp_p1p13) ─────────────
+`ifdef SM1P13_SUPPORTED
+    p1p13_state: coverpoint get_csr_val(ins.hart, ins.issue, `SAMPLE_CURRENT, "mstateen0", "p1p13") {
+        bins p1p13_disabled = {1'b0};
+        bins p1p13_enabled  = {1'b1};
+    }
+    hedelegh_csr: coverpoint ins.current.insn[31:20] {
+        wildcard bins hedelegh = {CSR_HEDELEGH};
+    }
+`endif
+
+    // ── Ssqosid-dependent coverpoints (cp_srmcfg) ────────────────────────
+`ifdef SSQOSID_SUPPORTED
+    `ifdef UDB_MXLEN_64
+        srmcfg_state: coverpoint ins.current.csr[CSR_MSTATEEN0][55] {
+                bins srmcfg_disabled = {1'b0};
+                bins srmcfg_enabled  = {1'b1};
+        }
+        srmcfg_csr: coverpoint ins.current.insn[31:20] {
+                wildcard bins srmcfg = {CSR_SRMCFG};
+        }
+    `else
+        srmcfg_state: coverpoint ins.current.csr[CSR_MSTATEEN0H][23] {
+                bins srmcfg_disabled = {1'b0};
+                bins srmcfg_enabled  = {1'b1};
+        }
+        srmcfg_csr: coverpoint ins.current.insn[31:20] {
+                wildcard bins srmcfg = {CSR_SRMCFG};
+        }
+    `endif
+`endif
+
+    // ── Sctr-dependent coverpoints (cp_ctr) ──────────────────────────────
+`ifdef SCTR_SUPPORTED
+    ctr_state: coverpoint get_csr_val(ins.hart, ins.issue, `SAMPLE_CURRENT, "mstateen0", "ctr") {
+        bins ctr_disabled = {1'b0};
+        bins ctr_enabled  = {1'b1};
+    }
+    ctr_csrs: coverpoint ins.current.insn[31:20] {
+        wildcard bins sctrdepth  = {CSR_SCTRDEPTH};
+        wildcard bins sctrstatus = {CSR_SCTRSTATUS};
+    }
+`endif
     cp_mstateen0_se0_controls_sstateen0: cross csrops, se0_state, sstateen_csrs {
             ignore_bins ig1 = binsof(sstateen_csrs.sstateen1);
             ignore_bins ig2 = binsof(sstateen_csrs.sstateen2);
@@ -110,7 +203,31 @@ covergroup Ssstateen_cg with function sample(ins_t ins);
         cp_jvt:              cross csrops, jvt_csr, jvt_state, se0_state;
         cp_jvt_lower_mode:   cross priv_mode_u, csrops, jvt_csr, jvt_state, se0_state;
     `endif
+        `ifdef SSDTRIG_SUPPORTED
+                cp_context: cross csrops, scontext_csr, context_state, priv_mode_s;
+        `endif
 
+        // Row 13: Sm1p13 + Hypervisor only
+        `ifdef SM1P13_SUPPORTED
+                cp_p1p13: cross csrops, p1p13_state, hedelegh_csr, priv_mode_s;
+        `endif
+
+        // Row 14: Ssqosid only
+        `ifdef SSQOSID_SUPPORTED
+                cp_srmcfg: cross csrops, srmcfg_csr, srmcfg_state, priv_mode_s;
+        `endif
+
+        // Row 15: Sctr only
+        `ifdef SCTR_SUPPORTED
+                cp_ctr: cross csrops, ctr_csrs, ctr_state, priv_mode_s;
+        `endif
+        `ifdef IMSIC_SUPPORTED
+                cp_imsic: cross csrops, imsic_csrs, imsic_state, priv_mode_s;
+        `endif
+        `ifdef AIA_SUPPORTED
+                cp_aia: cross csrops, aia_csrs, aia_state, priv_mode_s;
+        `endif
+        cp_envcfg: cross csrops, senvcfg_csr, envcfg_state, priv_mode_s;
 endgroup
 function void ssstateen_sample(int hart, int issue, ins_t ins);
     Ssstateen_cg.sample(ins);

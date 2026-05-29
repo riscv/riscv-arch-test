de40bd4bd71d193c9075e3bfa596a0430015eb92
karabambus <mradic07@gmail.com>
2026-05-29 13:26:42 +0200

    fix(cve4): wire cv32e40x interrupt/timer macros and bump sail max_time_to_wait
    InterruptsSm now triggers DUT irqs via mm_ram generator; align both cv32e40x configs.

--- config/cores/cve4/cv32e40x-rv32imc/rvmodel_macros.h
+++ config/cores/cve4/cv32e40x-rv32imc/rvmodel_macros.h
@@ -73,20 +73,32 @@
 ##### Machine Timer #####
 
 #define RVMODEL_TIMER_INT_SOON_DELAY 100
-/*
- * NOTE: CV32E40X has a time CSR (shadow of time_i input port).
- * MTIME and MTIMECMP are provided by the CLINT peripheral at 0x02000000.
- * Define the addresses below when the testbench wires up the CLINT.
- */
-//#define RVMODEL_MTIME_ADDRESS    0x02004000
-//#define RVMODEL_MTIMECMP_ADDRESS 0x02004008
+// CLINT machine timer in mm_ram at Sail's CLINT base (matches sail.json).
+#define RVMODEL_MTIME_ADDRESS     0x0200BFF8
+#define RVMODEL_MTIMECMP_ADDRESS  0x02004000
 
 ##### Machine Interrupts #####
 
-#define RVMODEL_SET_MEXT_INT(_R1, _R2)
-#define RVMODEL_CLR_MEXT_INT(_R1, _R2)
-#define RVMODEL_SET_MSW_INT(_R1, _R2)
-#define RVMODEL_CLR_MSW_INT(_R1, _R2)
+// Drive cv32e40x core irq pins via the cv32e40x-dv mm_ram Sail-protocol
+// simple_interrupt_generator at 0x15000020 (DUT testbench peripheral).
+//   base+4 (0x15000024): platform register, bit31 set(1)/clear(0),
+//   bit3 = MSI, bit11 = MEI.
+#define RVMODEL_SET_MEXT_INT(_R1, _R2)        \
+    li _R1, 0x80000800           ;            \
+    li _R2, 0x15000024           ;            \
+    sw _R1, 0(_R2)
+#define RVMODEL_CLR_MEXT_INT(_R1, _R2)        \
+    li _R1, 0x00000800           ;            \
+    li _R2, 0x15000024           ;            \
+    sw _R1, 0(_R2)
+#define RVMODEL_SET_MSW_INT(_R1, _R2)         \
+    li _R1, 0x80000008           ;            \
+    li _R2, 0x15000024           ;            \
+    sw _R1, 0(_R2)
+#define RVMODEL_CLR_MSW_INT(_R1, _R2)         \
+    li _R1, 0x00000008           ;            \
+    li _R2, 0x15000024           ;            \
+    sw _R1, 0(_R2)
 
 ##### Supervisor Interrupts #####
 

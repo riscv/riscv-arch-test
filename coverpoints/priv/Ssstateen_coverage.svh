8ccfda3d4212916eaf6dec9789ff971a85fc5ae3
ayeshaanwaar05 <ayesha.anwaar2005@gmail.com>
2026-05-31 12:35:57 -0700

    Removed features that are in Smstateen

--- coverpoints/priv/Ssstateen_coverage.svh
+++ coverpoints/priv/Ssstateen_coverage.svh
@@ -22,28 +22,31 @@ covergroup Ssstateen_cg with function sample(ins_t ins);
             bins sstateen2 = {CSR_SSTATEEN2};
             bins sstateen3 = {CSR_SSTATEEN3};
     }
+    sstateen_walk_csr: coverpoint ins.current.insn[31:20] {
+            bins sstateen0 = {CSR_SSTATEEN0};
+    }
 
     `ifdef UDB_MXLEN_64
     csr_walk: coverpoint ins.current.rs1_val {
             // bits [3:63] are WPRI; bit 0 (C) is readonly-zero
-            `ifdef ZFINX_SUPPORTED
+        `ifdef ZFINX_SUPPORTED
                 wildcard bins walking1_1  = {64'b??????????????????????????????????????????????????????????????1?};
-            `endif
-            `ifdef ZCMT_SUPPORTED
+        `endif
+        `ifdef ZCMT_SUPPORTED
                 wildcard bins walking1_2  = {64'b?????????????????????????????????????????????????????????????1??};
-            `endif
+        `endif
             wildcard bins walking0_1  = {64'b??????????????????????????????????????????????????????????????0?};
             wildcard bins walking0_2  = {64'b?????????????????????????????????????????????????????????????0??};
     }
     `else
     csr_walk: coverpoint ins.current.csr[ins.current.insn[31:20]] {
             // bits [3:31] are WPRI; bit 0 (C) is readonly-zero
-            `ifdef ZFINX_SUPPORTED
+        `ifdef ZFINX_SUPPORTED
                 wildcard bins walking1_1  = {32'b??????????????????????????????1?};
-            `endif
-            `ifdef ZCMT_SUPPORTED
+        `endif
+        `ifdef ZCMT_SUPPORTED
                 wildcard bins walking1_2  = {32'b?????????????????????????????1??};
-            `endif
+        `endif
             wildcard bins walking0_1  = {32'b??????????????????????????????0?};
             wildcard bins walking0_2  = {32'b?????????????????????????????0??};
     }
@@ -108,65 +111,6 @@ covergroup Ssstateen_cg with function sample(ins_t ins);
                 ignore_bins ig2 = binsof(misa_F.F_clear) && binsof(sstateen0_fcsr_bit.fcsr_zero);
         }
     `endif
-
-    `ifdef UDB_MXLEN_64
-        envcfg_state: coverpoint ins.current.csr[CSR_MSTATEEN0][62] {
-                bins envcfg_disabled = {1'b0};
-                bins envcfg_enabled  = {1'b1};
-        }
-    `else
-        envcfg_state: coverpoint ins.current.csr[CSR_MSTATEEN0H][30] {
-                bins envcfg_disabled = {1'b0};
-                bins envcfg_enabled  = {1'b1};
-        }
-    `endif
-    senvcfg_csr: coverpoint ins.current.insn[31:20] {
-            bins senvcfg = {CSR_SENVCFG};
-    }
-    `ifdef SSDTRIG_SUPPORTED
-    context_state: coverpoint get_csr_val(ins.hart, ins.issue, `SAMPLE_CURRENT, "mstateen0", "context") {
-        bins context_disabled = {1'b0};
-        bins context_enabled  = {1'b1};
-    }
-    scontext_csr: coverpoint ins.current.insn[31:20] {
-        wildcard bins scontext = {CSR_SCONTEXT};
-    }
-    `endif
-    `ifdef SM1P13_SUPPORTED
-    p1p13_state: coverpoint get_csr_val(ins.hart, ins.issue, `SAMPLE_CURRENT, "mstateen0", "p1p13") {
-        bins p1p13_disabled = {1'b0};
-        bins p1p13_enabled  = {1'b1};
-    }
-    hedelegh_csr: coverpoint ins.current.insn[31:20] {
-        wildcard bins hedelegh = {CSR_HEDELEGH};
-    }
-    `endif
-    `ifdef IMSIC_SUPPORTED
-        imsic_state: coverpoint get_csr_val(ins.hart, ins.issue, `SAMPLE_CURRENT, "mstateen0", "imsic") {
-            bins imsic_disabled = {1'b0};
-            bins imsic_enabled  = {1'b1};
-        }
-        imsic_csrs: coverpoint ins.current.insn[31:20] {
-            wildcard bins stopei = {CSR_STOPEI};
-            wildcard bins vstopei = {CSR_VSTOPEI};
-        }
-    `endif
-    `ifdef AIA_SUPPORTED
-        aia_state: coverpoint get_csr_val(ins.hart, ins.issue, `SAMPLE_CURRENT, "mstateen0", "aia") {
-            bins aia_disabled = {1'b0};
-            bins aia_enabled  = {1'b1};
-        }
-        aia_csrs: coverpoint ins.current.insn[31:20] {
-            `ifdef XLEN64
-                wildcard bins aia_m = {CSR_SIE};
-                wildcard bins aia_s = {CSR_SIP};
-            `endif
-            `ifdef XLEN32
-                bins aia_m = {CSR_SIEH};
-                bins aia_s = {CSR_SIPH};
-            `endif
-        }
-    `endif
     cp_mstateen0_se0_zero_controls_sstateen0: cross csrops, se0_zero, sstateen_csrs {
             ignore_bins ig1 = binsof(sstateen_csrs.sstateen1);
             ignore_bins ig2 = binsof(sstateen_csrs.sstateen2);
@@ -178,48 +122,11 @@ covergroup Ssstateen_cg with function sample(ins_t ins);
             ignore_bins ig3 = binsof(sstateen_csrs.sstateen3);
     }
     cp_csr_illegal_accesses: cross priv_mode_u, sstateen_csrs, csrops, se0_one;
-    cp_walking_ones:         cross sstateen_csrs, csrops, csr_walk, se0_one;
+    cp_walking_ones:         cross sstateen_walk_csr, csrops, csr_walk, se0_one;
     `ifdef ZCMT_SUPPORTED
         cp_jvt:              cross csrops, jvt_csr, jvt_state, se0_one;
         cp_jvt_lower_mode:   cross priv_mode_u, csrops, jvt_csr, jvt_state, se0_one;
     `endif
-
-        cp_envcfg: cross csrops, senvcfg_csr, envcfg_state, priv_mode_s;
-    `ifdef SSDTRIG_SUPPORTED
-        cp_context: cross csrops, scontext_csr, context_state, priv_mode_s;
-    `endif
-    `ifdef SM1P13_SUPPORTED
-        cp_p1p13: cross csrops, p1p13_state, hedelegh_csr, priv_mode_s;
-    `endif
-    `ifdef SCTR_SUPPORTED
-        cp_ctr: cross csrops, ctr_csrs, ctr_state, priv_mode_s;
-    `endif
-    `ifdef IMSIC_SUPPORTED
-        cp_imsic: cross csrops, imsic_csrs, imsic_state, priv_mode_s;
-    `endif
-    `ifdef AIA_SUPPORTED
-        cp_aia: cross csrops, aia_csrs, aia_state, priv_mode_s;
-    `endif
-    `ifdef SSQOSID_SUPPORTED
-        `ifdef UDB_MXLEN_64
-            srmcfg_state: coverpoint ins.current.csr[CSR_MSTATEEN0][55] {
-                    bins srmcfg_disabled = {1'b0};
-                    bins srmcfg_enabled  = {1'b1};
-            }
-            srmcfg_csr: coverpoint ins.current.insn[31:20] {
-                    wildcard bins srmcfg = {CSR_SRMCFG};
-            }
-        `else
-            srmcfg_state: coverpoint ins.current.csr[CSR_MSTATEEN0H][23] {
-                    bins srmcfg_disabled = {1'b0};
-                    bins srmcfg_enabled  = {1'b1};
-            }
-            srmcfg_csr: coverpoint ins.current.insn[31:20] {
-                    wildcard bins srmcfg = {CSR_SRMCFG};
-            }
-        `endif
-    `endif
-
 endgroup
 function void ssstateen_sample(int hart, int issue, ins_t ins);
     Ssstateen_cg.sample(ins);

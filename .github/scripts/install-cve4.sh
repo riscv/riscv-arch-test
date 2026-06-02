a484a937621996570f835aaae015324ad49f5d0a
karabambus <mradic07@gmail.com>
2026-06-02 15:28:53 +0200

    cve4: cv32e40x-dv CI to openhw main

--- .github/scripts/install-cve4.sh
+++ .github/scripts/install-cve4.sh
@@ -10,8 +10,8 @@ set -euo pipefail
 INSTALL_DIR="${1:?Usage: install-cve4.sh <install-dir>}"
 CVE4_DV_REPO="https://github.com/openhwgroup/cv32e40p-dv-review.git"
 CVE4_DV_COMMIT="1726d14796601884d54d9b0f699128800e2dcf55"
-CVE40X_DV_REPO="https://github.com/karabambus/cv32e40x-dv.git"
-CVE40X_DV_COMMIT="f6b3d6ebc825473458f9fc2714cfc0dce80e02a8"  # openhwgroup/cv32e40x-dv#86
+CVE40X_DV_REPO="https://github.com/openhwgroup/cv32e40x-dv.git"
+CVE40X_DV_COMMIT="93ce70f1ae2ab2e66716e0c5648d27e67e161c2f"
 CVE40X_CORE_HASH="18c88fd78a37f270c8301c552f5fd0f564d0ab20"  # pin cv32e40x RTL
 VERILATOR_VERSION="v5.042"
 

6f9778fb0ceb63739eb16ac8dfb629014105f85a
Jordan Carlin <jordanmcarlin@gmail.com>
2026-06-11 03:39:52 -0700

    Fix walking 0s

--- generators/testgen/src/testgen/asm/csr.py
+++ generators/testgen/src/testgen/asm/csr.py
@@ -319,7 +319,6 @@ def csr_walk_test(
                     "",
                     f"CSRW({csr_name}, x{temp_reg})      # set all bits",
                     f"CSRC({csr_name}, x{walk_reg})      # clear walking 1 in column {i}",
-                    test_data.add_testcase(f"{csr_name}_clr_bit_{i}", coverpoint, covergroup),
                 ]
             )
             reserved_fields = reserved_fields_written(i, walking_ones=False)
@@ -339,7 +338,12 @@ def csr_walk_test(
                     )
                 )
             else:
-                lines.append(gen_csr_read_sigupd(check_reg, csr, test_data, mask_reg))
+                lines.extend(
+                    [
+                        test_data.add_testcase(f"{csr_name}_clr_bit_{i}", coverpoint, covergroup),
+                        gen_csr_read_sigupd(check_reg, csr, test_data, mask_reg),
+                    ]
+                )
             lines.append(f"slli x{walk_reg}, x{walk_reg}, 1      # walk the 1")
         if need_endif:
             lines.append("#endif\n")

52c33c429e7d909f23701de9165389e091caeb7b
somiljain2006 <somil16022006@gmail.com>
2026-06-17 03:11:09 +0530

    Removal of redunadant line

--- generators/testgen/src/testgen/priv/extensions/InterruptsU.py
+++ generators/testgen/src/testgen/priv/extensions/InterruptsU.py
@@ -295,8 +295,7 @@ def _generate_user_wfi_timeout_tests(test_data: TestData) -> list[str]:
                         f"CSRS(mie, x{r_scratch})",
                     ]
                 )
-
-            # Clear timer (ensure no interrupt)                  
+               
             lines.append("# Clear timer (ensure no interrupt)")
             lines.extend(clr_mtimer_int(r_temp, r_mtimecmp))
 

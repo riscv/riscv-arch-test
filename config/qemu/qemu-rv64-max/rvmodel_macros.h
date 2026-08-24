ee95680b202f7748d745f04cfddf9f650becac47
ayeshaanwaar05 <ayesha.anwaar2005@gmail.com>
2026-08-24 05:05:59 -0700

    Adding tests of Sscofpmf
    Signed-off-by: ayeshaanwaar05 <ayesha.anwaar2005@gmail.com>

--- config/qemu/qemu-rv64-max/rvmodel_macros.h
+++ config/qemu/qemu-rv64-max/rvmodel_macros.h
@@ -263,4 +263,11 @@
 
 #define RVMODEL_CLR_SSW_INT(_R1, _R2)
 
+##### Sscofpmf #####
+#define RVMODEL_MHPMCOUNTER    mhpmcounter3
+#define RVMODEL_MHPMEVENT      mhpmevent3
+#define RVMODEL_MHPMEVENT_VAL  0x02   // RISCV_PMU_EVENT_HW_INSTRUCTIONS
+#define RVMODEL_MHPMEVENT_CODE(_R1, _R2) \
+    nop
+
 #endif // _RVMODEL_MACROS_H

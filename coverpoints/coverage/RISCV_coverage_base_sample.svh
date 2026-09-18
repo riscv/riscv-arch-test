0e3d8c7a9a8b8e109bb320d770d755a00ef57d20
Ellen Yu <ellyu@g.hmc.edu>
2026-09-17 19:56:46 -0700

    merge
    Signed-off-by: Ellen Yu <ellyu@g.hmc.edu>

--- coverpoints/coverage/RISCV_coverage_base_sample.svh
+++ coverpoints/coverage/RISCV_coverage_base_sample.svh
@@ -118,9 +118,6 @@
     `ifdef COVER_INTERRUPTSSM
         interruptssm_sample(hart, issue, ins);
     `endif
-    `ifdef COVER_INTERRUPTSSM2
-        interruptssm2_sample(hart, issue, ins);
-    `endif
     `ifdef COVER_INTERRUPTSSSTC
         interruptssstc_sample(hart, issue, ins);
     `endif
